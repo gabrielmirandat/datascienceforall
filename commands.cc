787e767f873aa7a1d142c7ee7f791fba196eeab9
Gabriel Miranda <gabrielmirandatt@gmail.com>
2018-03-14 13:38:31 -0300

    Second part import data

--- commands.cc
+++ commands.cc
@@ -1,4 +1,4 @@
-import data 1
+# import data 1
 
 dir() // lista diretorio atual
 
@@ -8,10 +8,19 @@ data <- read.delim("states.txt", stringsAsFactors = FALSE) // separado tab
 data <- read.table("states2.txt", header=TRUE, sep="/ ou \t", stringsAsFactors = FALSE) // separado por / ou outra coisa
 
 data // printa var
-str(data) // printa var
+str(data) // printa estrutura da var
 summary(data) // info estatistica
 head(data) // primeiras seis observacoes
 
 // Select the hot dog with the least calories: lily
 lily <- hotdogs[which.min(hotdogs$calories), ]
+
+# import data 2
+
+install.packages("readr")
+library(readr)
+
+potatoes_fragment <- read_tsv("potatoes.txt", skip = 6, n_max = 5, col_names = properties)
+potatoes <- fread("potatoes.csv")
+plot(tasty_potatoes$texture, tasty_potatoes$moistness)
 

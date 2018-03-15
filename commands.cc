e80746f3c89819061effd2e944f21bc759f1d99d
Gabriel Miranda <gabrielmirandatt@gmail.com>
2018-03-15 13:52:17 -0300

    Finished importing data in r 1

--- commands.cc
+++ commands.cc
@@ -24,3 +24,26 @@ potatoes_fragment <- read_tsv("potatoes.txt", skip = 6, n_max = 5, col_names = p
 potatoes <- fread("potatoes.csv")
 plot(tasty_potatoes$texture, tasty_potatoes$moistness)
 
+# import data 3
+
+library(readxl)
+excel_sheets("cities.xlsx")
+read_excel("cities.xlsx")
+
+pop_list <- lapply(excel_sheets("urbanpop.xlsx"),
+                   read_excel,
+                   path = "urbanpop.xlsx")
+
+pop_a <- read_excel("latitude_nonames.xlsx", sheet=1, col_names=FALSE)
+
+head(urban_pop, n=11)
+
+# import data 4
+library(XLConnect)
+my_book <- loadWorkbook("urbanpop.xlsx")
+class(my_book)
+
+writeWorksheet(my_book,sheet="data_summary",summ)
+saveWorkbook(my_book, file ="summary.xlsx")
+removeSheet(book, sheet = "Y2010")
+saveWorkbook(book, file = "cities4.xlsx")

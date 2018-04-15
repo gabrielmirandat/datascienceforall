69b1d01d0da4cd545d407bae634a103ac0074f95
gabrielmirandat <gabrielmirandatt@gmail.com>
2018-04-14 22:18:26 -0300

    Added data vis with ggplot

--- commands.cc
+++ commands.cc
@@ -524,10 +524,198 @@ ggplot(mtcars, aes(x = wt, y = mpg, fill = cyl)) +
 ggplot(mtcars, aes(x = mpg, y = qsec, col = factor(cyl), shape = factor(am), size = (hp/wt))) +
   geom_point()
 
-  
+# The base layer, cyl.am, is available for you
+# Add geom (position = "stack" by default)
+cyl.am + 
+  geom_bar()
 
+# Fill - show proportion
+cyl.am + 
+  geom_bar(position = "fill")  
+
+# Dodging - principles of similarity and proximity
+cyl.am +
+  geom_bar(position = "dodge") 
+
+# Clean up the axes with scale_ functions
+val = c("#E41A1C", "#377EB8")
+lab = c("Manual", "Automatic")
+cyl.am +
+  geom_bar(position = "dodge") +
+  scale_x_discrete("Cylinders") + 
+  scale_y_continuous("Number") +
+  scale_fill_manual("Transmission", 
+                    values = val,
+                    labels = lab)
+
+# 1 - Create jittered plot of mtcars, mpg onto x, 0 onto y
+ggplot(mtcars, aes(x = mpg, y = 0)) +
+  geom_jitter()
+
+# 2 - Add function to change y axis limits
+ggplot(mtcars, aes(x = mpg, y = 0)) +
+  geom_jitter() +
+  scale_y_continuous(limits=c(-2, 2))
+
+# Basic scatter plot: wt on x-axis and mpg on y-axis; map cyl to col
+ggplot(mtcars, 
+aes(x = wt, y = mpg, col = cyl)) +
+geom_point(size=4)
+
+
+# Hollow circles - an improvement
+ggplot(mtcars, 
+aes(x = wt, y = mpg, col = cyl)) +
+geom_point(size=4, shape=1)
+
+
+# Add transparency - very nice
+ggplot(mtcars, 
+aes(x = wt, y = mpg, col = cyl)) +
+geom_point(alpha=0.6, size = 4)
+
+# Scatter plot: carat (x), price (y), clarity (color)
+ggplot(diamonds, 
+aes(x = carat, y = price, col = clarity)) +
+geom_point()
+
+
+# Adjust for overplotting
+ggplot(diamonds, 
+aes(x = carat, y = price, col = clarity)) +
+geom_point(alpha = 0.5)
+
+
+# Scatter plot: clarity (x), carat (y), price (color)
+ggplot(diamonds, 
+aes(x = clarity, y = carat, col = price)) +
+geom_point(alpha = 0.5)
+
+
+# Dot plot with jittering
+ggplot(diamonds, 
+aes(x = clarity, y = carat, col = price)) +
+geom_point(alpha = 0.5, position = "jitter")
 
 # PART 4
+
+# 2 - Set width in geom_jitter()
+ggplot(mtcars, aes(x = cyl, y = wt)) +
+  geom_jitter(width = 0.1)
+
+# 3 - Set position = position_jitter() in geom_point() ()
+ggplot(mtcars, aes(x = cyl, y = wt)) +
+  geom_point(position = position_jitter(0.1))
+
+# 1 - Make a univariate histogram
+ggplot(mtcars, aes(x=mpg)) +
+   geom_histogram()
+
+# 2 - Plot 1, plus set binwidth to 1 in the geom layer
+ggplot(mtcars, aes(x=mpg)) +
+   geom_histogram(binwidth = 1)
+
+
+# 3 - Plot 2, plus MAP ..density.. to the y aesthetic (i.e. in a second aes() function)
+ggplot(mtcars, aes(x=mpg)) +
+   geom_histogram(binwidth = 1, aes(y=..density..))
+
+
+
+# 4 - plot 3, plus SET the fill attribute to "#377EB8"
+ggplot(mtcars, aes(x=mpg)) +
+   geom_histogram(binwidth = 1,fill="#377EB8", aes(y=..density..))
+
+
+# Draw a bar plot of cyl, filled according to am
+ggplot(mtcars, aes(x = cyl, fill = am)) +
+  geom_bar()
+
+# Change the position argument to stack
+ggplot(mtcars, aes(x = cyl, fill = am)) +
+  geom_bar(position="stack")
+
+
+# Change the position argument to fill
+ggplot(mtcars, aes(x = cyl, fill = am)) +
+  geom_bar(position="fill")
+
+
+# Change the position argument to dodge
+ggplot(mtcars, aes(x = cyl, fill = am)) +
+  geom_bar(position="dodge")
+
+ggplot(mtcars, aes(mpg,col=cyl)) +
+  geom_freqpoly(binwidth=1)  
+
+# Example of how to use a brewed color palette
+ggplot(mtcars, aes(x = cyl, fill = am)) +
+  geom_bar() +
+  scale_fill_brewer(palette = "Set1")
+
+# Use str() on Vocab to check out the structure
+str(Vocab)
+
+# Plot education on x and vocabulary on fill
+# Use the default brewed color palette
+ggplot(Vocab, aes(x=education, fill=vocabulary)) +
+   geom_bar(position = "fill") + 
+   scale_fill_brewer()
+
+# Definition of a set of blue colors
+blues <- brewer.pal(9, "Blues") # from the RColorBrewer package
+
+# 1 - Make a color range using colorRampPalette() and the set of blues
+blue_range <- colorRampPalette(blues)
+
+# 2 - Use blue_range to adjust the color of the bars, use scale_fill_manual()
+ggplot(Vocab, aes(x = education, fill = vocabulary)) +
+  geom_bar(position = "fill") +
+  scale_fill_manual(values = blue_range(11))
+
+# 6 - Plot 5, plus change mapping: cyl onto fill
+ggplot(mtcars, aes(mpg, fill = cyl)) +
+  geom_histogram(binwidth = 1, position = "identity", alpha=0.4)
+
+SCATTER = points, jitter, abline
+BAR = histogram, bar, errorbar
+LINE = line     
+
+# Adjust plot to represent the fraction of total population that is unemployed
+ggplot(economics, aes(x = date, y = unemploy/pop)) +
+geom_line()
+
+# Expand the following command with geom_rect() to draw the recess periods
+ggplot(economics, aes(x = date, y = unemploy/pop)) +
+  geom_rect(data = recess,
+         aes(xmin = begin, xmax = end, ymin = -Inf, ymax = +Inf),
+         inherit.aes = FALSE, fill = "red", alpha = 0.2) +
+  geom_line()
+
+ggplot(fish.tidy, aes(x = Year, y = Capture, col = Species)) +
+geom_line()
+
 # PART 5
 
-linkedin
+qplot(wt, mpg, data = mtcars)
+qplot(factor(cyl), factor(vs), data = mtcars, geom="jitter")
+
+# 2 - qplot with geom "dotplot", binaxis = "y" and stackdir = "center"
+qplot(
+  cyl, wt,
+  data = mtcars,
+  fill = am,
+  geom = "dotplot",
+  binaxis = "y",
+  stackdir = "center"
+)
+
+# 4 - Take plot 3, add geom_smooth()
+ggplot(ChickWeight, aes(x = Time, y = weight,color=Diet)) +
+  geom_line(aes(group = Chick, alpha=0.3)) + 
+  geom_smooth(lwd=2, se=FALSE)
+
+# 5 - Plot 3, but use the position object from instruction 4
+ggplot(titanic, aes(x = Pclass, y=Age, col = Sex)) +
+  geom_point(size = 3, alpha = 0.5 ,position = posn.jd)+
+  facet_grid(. ~ Survived)
